dfc1d2f3250e43ab18342c970fe761a70b4c5fa1
Sadia Afrin Tarin <tarin.cse5.bu@gmail.com>
2023-05-20 21:14:18 +0600

    up/down time monitor with worker

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-/*
- * Title: Uptime Monitoring Application
- * Description: A RESTFul API to monitor up or down time of user defined links
+/* Title: Project initial file
+ * Description: Initial file to start the node server and file
  * Author: Sadia Afrin Tarin ( Inspired by Learn with Sumit )
  * Date: 5/5/2023
  *
  */
 // dependencies
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments');
-const {sendTwilioSms} = require('./helpers/notifications');
+const server = require('./lib/server');
+const workers = require('./lib/worker');
+
+
 
 // app object - module scaffolding
 const app = {};
 
-sendTwilioSms("01318777906",'FROM Uptime Monitoring System, Hi Tarin',(err)=>{
-    console.log(`this is the error`,err);
-});
-// create server
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environment.port, () => {
-        console.log(`listening to port ${environment.port}`);
-    });
+app.init = () => {
+    // start the server
+    server.init();
+    // start the workers
+    workers.init();
 };
 
-// handle Request Response
-app.handleReqRes = handleReqRes;
+app.init();
 
-// start the server
-app.createServer();
+// export the app
+module.exports = app;
